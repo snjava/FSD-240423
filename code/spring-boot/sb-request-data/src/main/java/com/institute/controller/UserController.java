f29e094690959fb1663de3a7a36a04ce68256daf
snjava <scnaikwadi2@gmail.com>
2023-07-26 08:38:08 +0530

    Spring Boot Request Data
    Spring Boot Request Data

---
+++ code/spring-boot/sb-request-data/src/main/java/com/institute/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.institute.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	// Accept data from request in the form of parameter
+	// http://localhost:8080/user-data-1?fname=Abc&mname=Pqr&lname=Xyz
+	@GetMapping("/user-data-1")
+	public String userDetails1(@RequestParam("fname") String fn, @RequestParam("mname") String mn, @RequestParam("lname") String ln) {
+		String fullName = fn + " " + mn + " " + ln;
+		return "Hello, " + fullName;
+	}
+	
+	// Accept data from request in the form of path variable
+	// http://localhost:8080/user-data-2/Abc/Pqr/Xyz
+	@GetMapping("/user-data-2/{fname}/{mname}/{lname}")
+	public String userDetails2(@PathVariable(name = "fname") String fn, @PathVariable(name = "mname") String mn, 
+			@PathVariable(name = "lname") String ln) {
+		String fullName = fn + " " + mn + " " + ln;
+		return "Hello, " + fullName;
+	}
+	
+}
+
